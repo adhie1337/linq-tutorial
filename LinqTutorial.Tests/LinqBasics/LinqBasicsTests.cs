3e6a7e9029ad53cb35270982a197e49e44a19249
adhie1337 <adhie@inf.elte.hu>
2021-05-24 22:32:45 +0200

    Rename test file

--- LinqTutorial.Tests/LinqBasics/LinqBasicsTests.cs
+++ LinqTutorial.Tests/LinqBasics/LinqBasicsTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LinqTutorial.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class LinqBasicsTests
     {
         [TestMethod]
         public void TestMethod1()
