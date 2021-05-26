723e194db2c8d15504e0dc17a2a791f3c4f8148c
adhie1337 <adhie@inf.elte.hu>
2021-05-26 20:30:22 +0200

    Fix test

--- LinqTutorial.Implementation/Enumerables/MyEnumerable.cs
+++ LinqTutorial.Implementation/Enumerables/MyEnumerable.cs
@@ -15,14 +15,40 @@ namespace LinqTutorial.Implementation.Enumerables
 
     public class MySelectEnumerable<TSource, TResult> : IEnumerable<TResult>
     {
-        public IEnumerator<TResult> GetEnumerator() => throw new NotImplementedException();
+        private IEnumerable<TSource> source;
+        private Func<TSource, TResult> resultSelector;
+
+        public MySelectEnumerable(IEnumerable<TSource> source, Func<TSource, TResult> resultSelector)
+        {
+            this.source = source;
+            this.resultSelector = resultSelector;
+        }
+
+        public IEnumerator<TResult> GetEnumerator() => new MySelectEnumerator<TSource, TResult>(source.GetEnumerator(), resultSelector);
 
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 
+        private readonly struct MySelectEnumerator<TS, TR> : IEnumerator<TR>
+        {
+            private readonly IEnumerator<TS> _source;
+            private readonly Func<TS, TR> _resultSelector;
+
+            public TR Current => this._resultSelector(this._source.Current);
+
+            object IEnumerator.Current => this.Current;
+
+            public MySelectEnumerator(IEnumerator<TS> source, Func<TS, TR> resultSelector) => (this._source, this._resultSelector) = (source, resultSelector);
+
+            public bool MoveNext() => this._source.MoveNext();
+
+            public void Reset() => this._source.Reset();
+
+            public void Dispose() { }
+        }
     }
 
     public static class EnumerableExtensions
     {
-        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> resultSelector) => throw new NotImplementedException();
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> resultSelector) => new MySelectEnumerable<TSource, TResult>(source, resultSelector);
     }
 }
