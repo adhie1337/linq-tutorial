960b62fe72f590aab982b72c32ab6ab24b0e6373
adhie1337 <adhie@inf.elte.hu>
2021-05-26 19:50:06 +0200

    Add failing test for excercise-enumerables-001

--- LinqTutorial.Tests/Enumerables/MyEnumerablesTests.cs
+++ LinqTutorial.Tests/Enumerables/MyEnumerablesTests.cs
@@ -6,12 +6,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LinqTutorial.Tests.Enumerables
 {
-    /*
-    Your task is to implement `MyEnumerable` that is an implementor of `IEnumerable<int>`. It has a static method that is almost the same as `Enumerable.Range`, but `Enumerable.Range` takes two parameters: the start int, and the number of items to generate, but `MyEnumerable.Range` takes a start int and an end in exclusive.
-    */
     [TestClass]
     public class MyEnumerablesTests
     {
+        /*
+        Your task is to implement `MyEnumerable` that is an implementor of `IEnumerable<int>`. It has a static method that is almost the same as `Enumerable.Range`, but `Enumerable.Range` takes two parameters: the start int, and the number of items to generate, but `MyEnumerable.Range` takes a start int and an end in exclusive.
+        */
         [TestMethod]
         public void MyEnumerable_GeneratesInts()
         {
