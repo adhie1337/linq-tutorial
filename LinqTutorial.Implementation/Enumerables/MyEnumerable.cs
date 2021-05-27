a97da0cf3fe6e0a3c0adee89fcf13c3b2d23e787
adhie1337 <adhie@inf.elte.hu>
2021-05-27 18:39:58 +0200

    Fix test

--- LinqTutorial.Implementation/Enumerables/MyEnumerable.cs
+++ LinqTutorial.Implementation/Enumerables/MyEnumerable.cs
@@ -22,15 +22,46 @@ namespace LinqTutorial.Implementation.Enumerables
 
     public class MyWhereEnumerable<TSource> : IEnumerable<TSource>
     {
-        public IEnumerator<TSource> GetEnumerator() => throw new NotImplementedException();
+        private readonly IEnumerable<TSource> _source;
+        private readonly Predicate<TSource> _predicate;
+
+        public MyWhereEnumerable(IEnumerable<TSource> source, Predicate<TSource> predicate) => (this._source, this._predicate) = (source, predicate);
+
+        public IEnumerator<TSource> GetEnumerator() => new MyWhereEnumerator<TSource>(this._source.GetEnumerator(), this._predicate);
 
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private struct MyWhereEnumerator<TS> : IEnumerator<TS>
+        {
+            private readonly IEnumerator<TS> _source;
+            private readonly Predicate<TS> _predicate;
+
+            public MyWhereEnumerator(IEnumerator<TS> source, Predicate<TS> predicate) => (this._source, this._predicate) = (source, predicate);
+
+            public TS Current => this._source.Current;
+
+            object IEnumerator.Current => this.Current;
+
+            public bool MoveNext()
+            {
+                do
+                {
+                    if (!this._source.MoveNext()) return false;
+                } while (!this._predicate(this._source.Current));
+
+                return true;
+            }
+
+            public void Reset() => this._source.Reset();
+
+            public void Dispose() => this._source.Dispose();
+        }
     }
 
     public static class EnumerableExtensions
     {
         public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> resultSelector) => throw new NotImplementedException();
 
-        public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> filter) => throw new NotImplementedException();
+        public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Predicate<TSource> predicate) => new MyWhereEnumerable<TSource>(source, predicate);
     }
 }
